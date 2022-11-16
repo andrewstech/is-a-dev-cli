72192a49dbb07d3fae00c1f9cab540314bc993cf
William Harrison <william@williamdavidharrison.com.au>
2022-11-16 08:49:20 +0800

    prettier

--- functions/logout.js
+++ functions/logout.js
@@ -1,14 +1,15 @@
-const Conf = require('conf');
+const Conf = require("conf");
 const account = new Conf();
 
 function logout() {
-  if (!account.has('username')) {
-    console.log('You are not logged in.');
+  if (!account.has("username")) {
+    console.log("You are not logged in.");
     return;
   }
-  account.delete('username');
-  account.delete('token');
-  console.log('You have been logged out.');
+
+  account.delete("username");
+  account.delete("token");
+  console.log("You have been logged out.");
 }
 
 module.exports = logout;
