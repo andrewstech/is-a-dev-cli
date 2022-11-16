a93ad1ea257b23a8edce8868e8527991d6d1a747
William Harrison <william@williamdavidharrison.com.au>
2022-11-16 08:46:42 +0800

    prettier

--- functions/createquestions.js
+++ functions/createquestions.js
@@ -1,25 +1,25 @@
 const questions = [
   {
-    type: 'text',
-    name: 'subdomain',
-    message: 'What subdomain would you like?',
+    type: "text",
+    name: "subdomain",
+    message: "What subdomain would you like?",
   },
   {
-    type: 'select',
-    name: 'type',
-    message: 'What Type of Record is required?',
+    type: "select",
+    name: "type",
+    message: "What type of record do you want to use?",
     choices: [
-      { value: 'A' },
-      { value: 'CNAME' },
-      { value: 'MX' },
-      { value: 'TXT' }
+      { value: "A" },
+      { value: "CNAME" },
+      { value: "MX" },
+      { value: "TXT" },
     ],
   },
   {
-    type: 'text',
-    name: 'Content',
-    message: 'What is the content of the record?',
-  }
+    type: "text",
+    name: "Content",
+    message: "What is the content of the record?",
+  },
 ];
 
 module.exports = questions;
