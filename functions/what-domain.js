24ff8fd169b2967effdd7e55c6dfbf956c007f60
William Harrison <william@williamdavidharrison.com.au>
2022-11-16 08:49:53 +0800

    prettier

--- functions/what-domain.js
+++ functions/what-domain.js
@@ -1,9 +1,9 @@
 const wquestions = [
-    {
-      type: 'text',
-      name: 'domain',
-      message: 'What is the domain you wish to check?',
-    }
-    ];
+  {
+    type: "text",
+    name: "domain",
+    message: "What is the domain you wish to check?",
+  },
+];
 
 module.exports = wquestions;
