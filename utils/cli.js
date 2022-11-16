0136fc49044f15f869b94511c2ac76f804b60984
William Harrison <william@williamdavidharrison.com.au>
2022-11-16 08:50:55 +0800

    prettier

--- utils/cli.js
+++ utils/cli.js
@@ -1,55 +1,55 @@
-const meow = require('meow');
-const meowHelp = require('cli-meow-help');
+const meow = require("meow");
+const meowHelp = require("cli-meow-help");
 
 const flags = {
-	clear: {
-		type: `boolean`,
-		default: true,
-		alias: `c`,
-		desc: `Clear the console`
-	},
-	noClear: {
-		type: `boolean`,
-		default: true,
-		desc: `Don't clear the console`
-	},
-	debug: {
-		type: `boolean`,
-		default: false,
-		alias: `d`,
-		desc: `Print debug info`
-	},
-	version: {
-		type: `boolean`,
-		alias: `v`,
-		desc: `Print CLI version`
-	},
-	check: {
-		type: `string`,
-		alias: `c`,
-		desc: `Check if a subdomain is available`
-	},
+  clear: {
+    type: `boolean`,
+    default: true,
+    alias: `c`,
+    desc: `Clear the console`,
+  },
+  noClear: {
+    type: `boolean`,
+    default: true,
+    desc: `Don't clear the console`,
+  },
+  debug: {
+    type: `boolean`,
+    default: false,
+    alias: `d`,
+    desc: `Print debug info`,
+  },
+  version: {
+    type: `boolean`,
+    alias: `v`,
+    desc: `Print CLI version`,
+  },
+  check: {
+    type: `string`,
+    alias: `c`,
+    desc: `Check if a subdomain is available`,
+  },
 };
 
 const commands = {
-	create: { desc: `Create a subdomain` },
-	check: { desc: `Check if a subdomain is available` },
-	login: { desc: `Login to is-a.dev` },
-	help: { desc: `Print help info` },
-	logout: { desc: `Logout of is-a.dev` }
+  create: { desc: `Create a subdomain` },
+  check: { desc: `Check if a subdomain is available` },
+  login: { desc: `Login to is-a.dev` },
+  help: { desc: `Print help info` },
+  logout: { desc: `Logout of is-a.dev` },
 };
 
 const helpText = meowHelp({
-	name: `is-a-dev`,
-	flags,
-	commands
+  name: `is-a-dev`,
+  flags,
+  commands,
 });
 
 const options = {
-	inferType: true,
-	description: false,
-	hardRejection: false,
-	flags
+  inferType: true,
+  description: false,
+  hardRejection: false,
+  flags,
 };
 
 module.exports = meow(helpText, options);
