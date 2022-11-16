c8aed9e224309f5cb751380d7464b1ee280a49b6
William Harrison <william@williamdavidharrison.com.au>
2022-11-16 08:52:38 +0800

    prettier

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-#!/usr/bin/env node
-
 /**
  * is-a-dev
- * Create Free subdomains
+ * Create free subdomains
  *
  * @author andrewstech <httpz>
  */
 
-const init = require('./utils/init');
-const cli = require('./utils/cli');
-const log = require('./utils/log');
-const login = require('./functions/login');
-const check = require('./functions/check');
-const logout = require('./functions/logout');
-const create = require('./functions/create');
-
+const init = require("./utils/init");
+const cli = require("./utils/cli");
+const log = require("./utils/log");
+const login = require("./functions/login");
+const check = require("./functions/check");
+const logout = require("./functions/logout");
+const create = require("./functions/create");
 
 const input = cli.input;
 const flags = cli.flags;
 const { clear, debug } = flags;
 
 (async () => {
-	init({ clear });
-	input.includes(`help`) && cli.showHelp(0);
-	input.includes(`login`) && login();
-	input.includes(`check`) && check(flags.check);
-	input.includes(`logout`) && logout();
-	input.includes(`create`) && create();
+  init({ clear });
+  input.includes(`help`) && cli.showHelp(0);
+  input.includes(`login`) && login();
+  input.includes(`check`) && check(flags.check);
+  input.includes(`logout`) && logout();
+  input.includes(`create`) && create();
 
-	debug && log(flags);
+  debug && log(flags);
 })();
