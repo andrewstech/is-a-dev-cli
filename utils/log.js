f21614783d200842062061c14624ae400d98f0f4
Levi R <levir620@skiff.com>
2023-01-06 22:42:45 -0800

    🧑‍💻 Add helper log functions

--- utils/log.js
+++ utils/log.js
@@ -1,12 +1,30 @@
 const alert = require('cli-alerts');
 
-module.exports = info => {
-	alert({
+const info = () => {
+	const log = alert({
 		type: `warning`,
 		name: `DEBUG LOG`,
 		msg: ``
 	});
 
-	console.log(info);
-	console.log();
+	console.log(log, "\n");
+};
+
+const positive = (message) => {
+	console.log(`\x1b[32m%s\x1b[0m`, message);
+};
+
+const neutral = (message) => {
+	console.log(message);
+};
+
+const negative = (message) => {
+	console.log(`\x1b[31m%s\x1b[0m`, message);
+};
+
+module.exports = {
+	info,
+	positive,
+	neutral,
+	negative
 };
